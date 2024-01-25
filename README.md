161e64f6e1b323dfb54a9c7d75710ee4240cc023
Paul <budkudyba@gmail.com>
2024-01-25 14:44:14 -0500

    added foo, etc

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # NO_FEC
 This is an exploration of a GNU_Radio 802.11 without Forward Error Correction
+
+## install instructions
+* install radioconda and mamba
+    * https://github.com/ryanvolz/radioconda
+* create a new env (no_fec)
+```
+conda create -n no_fec 
+```
+* install gnuradio and dependencies
+```
+mamba install gnuradio gnuradio-build-deps boost
+```
+* reopen env
+```
+conda activate no_fec
+```
+* build/install gr-foo (gr-foo-maint-3.10)
+### build/install commands
+```
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DLIB_SUFFIX="" ..
+cmake --build .
+cmake --build . --target install
+```
+* build/install NO-FEC (gr-ieee802-11-maint-3.10_NO-FEC)
+    *same build/install steps as above
+
+
+
--- gnuradio_companion/wifi_loopback.py
+++ gnuradio_companion/wifi_loopback.py
@@ -6,49 +6,36 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Wifi Loopback
-# GNU Radio version: 3.10.1.1
+# GNU Radio version: 3.10.9.2
 
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
+from PyQt5 import Qt
+from gnuradio import qtgui
 import os
 import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
-from PyQt5 import Qt
+from PyQt5 import QtCore
 from PyQt5.QtCore import QObject, pyqtSlot
-from gnuradio import qtgui
-import sip
 from gnuradio import blocks
+from gnuradio import blocks, gr
 from gnuradio import channels
 from gnuradio.filter import firdes
 from gnuradio import gr
 from gnuradio.fft import window
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import gr, pdu
 from gnuradio import zeromq
 from gnuradio.filter import pfb
-from gnuradio.qtgui import Range, RangeWidget
-from PyQt5 import QtCore
 from wifi_phy_hier import wifi_phy_hier  # grc-generated hier_block
 import foo
 import ieee802_11
+import sip
 
 
-
-from gnuradio import qtgui
 
 class wifi_loopback(gr.top_block, Qt.QWidget):
 
@@ -59,8 +46,8 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -76,12 +63,11 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "wifi_loopback")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -98,22 +84,23 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._snr_range = Range(-15, 30, 0.1, 30, 200)
-        self._snr_win = RangeWidget(self._snr_range, self.set_snr, "'snr'", "counter_slider", float, QtCore.Qt.Horizontal)
+
+        self._snr_range = qtgui.Range(-15, 30, 0.1, 30, 200)
+        self._snr_win = qtgui.RangeWidget(self._snr_range, self.set_snr, "'snr'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._snr_win, 2, 0, 1, 1)
         for r in range(2, 3):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self._noise_range = Range(0, 1, 0.1, 0, 200)
-        self._noise_win = RangeWidget(self._noise_range, self.set_noise, "'noise'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self._noise_range = qtgui.Range(0, 1, 0.1, 0, 200)
+        self._noise_win = qtgui.RangeWidget(self._noise_range, self.set_noise, "'noise'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._noise_win, 4, 0, 1, 1)
         for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self._epsilon_range = Range(-20e-6, 20e-6, 1e-6, 0, 200)
-        self._epsilon_win = RangeWidget(self._epsilon_range, self.set_epsilon, "'epsilon'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self._epsilon_range = qtgui.Range(-20e-6, 20e-6, 1e-6, 0, 200)
+        self._epsilon_win = qtgui.RangeWidget(self._epsilon_range, self.set_epsilon, "'epsilon'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._epsilon_win, 3, 1, 1, 1)
         for r in range(3, 4):
             self.top_grid_layout.setRowStretch(r, 1)
@@ -179,14 +166,14 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
             sensitivity=0.56,
         )
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
-            48*10, #size
+            (48*10), #size
             "", #name
             1, #number of inputs
             None # parent
         )
         self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
-        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_y_axis((-2), 2)
+        self.qtgui_const_sink_x_0.set_x_axis((-2), 2)
         self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
         self.qtgui_const_sink_x_0.enable_autoscale(False)
         self.qtgui_const_sink_x_0.enable_grid(False)
@@ -224,20 +211,21 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         for c in range(0, 2):
             self.top_grid_layout.setColumnStretch(c, 1)
         self.pfb_arb_resampler_xxx_0 = pfb.arb_resampler_ccf(
-            1+epsilon,
+            (1+epsilon),
             taps=None,
-            flt_size=32)
+            flt_size=32,
+            atten=100)
         self.pfb_arb_resampler_xxx_0.declare_sample_delay(0)
         self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.complex_t, 'packet_len')
-        self._pdu_length_range = Range(0, 1500, 1, 64, 200)
-        self._pdu_length_win = RangeWidget(self._pdu_length_range, self.set_pdu_length, "'pdu_length'", "counter_slider", int, QtCore.Qt.Horizontal)
+        self._pdu_length_range = qtgui.Range(0, 1500, 1, 64, 200)
+        self._pdu_length_win = qtgui.RangeWidget(self._pdu_length_range, self.set_pdu_length, "'pdu_length'", "counter_slider", int, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._pdu_length_win, 3, 0, 1, 1)
         for r in range(3, 4):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self._interval_range = Range(10, 1000, 1, 1000, 200)
-        self._interval_win = RangeWidget(self._interval_range, self.set_interval, "'interval'", "counter_slider", int, QtCore.Qt.Horizontal)
+        self._interval_range = qtgui.Range(10, 1000, 1, 1000, 200)
+        self._interval_win = qtgui.RangeWidget(self._interval_range, self.set_interval, "'interval'", "counter_slider", int, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._interval_win, 2, 1, 1, 1)
         for r in range(2, 3):
             self.top_grid_layout.setRowStretch(r, 1)
@@ -246,10 +234,10 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         self.ieee802_11_parse_mac_0 = ieee802_11.parse_mac(False, True)
         self.ieee802_11_mac_0 = ieee802_11.mac([0x23, 0x23, 0x23, 0x23, 0x23, 0x23], [0x42, 0x42, 0x42, 0x42, 0x42, 0x42], [0xff, 0xff, 0xff, 0xff, 0xff, 0xff])
         self.foo_packet_pad2_0 = foo.packet_pad2(False, True, 0.001, 500, 0)
-        self.foo_packet_pad2_0.set_min_output_buffer(960000)
+        self.foo_packet_pad2_0.set_min_output_buffer((out_buf_size * 10))
         self.channels_channel_model_0 = channels.channel_model(
             noise_voltage=noise,
-            frequency_offset=epsilon * 5.89e9 / 10e6,
+            frequency_offset=(epsilon * 5.89e9 / 10e6),
             epsilon=1.0,
             taps=[1.0],
             noise_seed=0,
@@ -263,7 +251,7 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
         self.blocks_msgpair_to_var_0_0_0_0 = blocks.msg_pair_to_var(self.set_encoding)
         self.blocks_msgpair_to_var_0_0_0 = blocks.msg_pair_to_var(self.set_epsilon)
         self.blocks_msgpair_to_var_0_0 = blocks.msg_pair_to_var(self.set_snr)
-        self.blocks_message_debug_0 = blocks.message_debug(True)
+        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
 
 
         ##################################################
@@ -339,8 +327,8 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
     def set_epsilon(self, epsilon):
         self.epsilon = epsilon
         self.blocks_var_to_msg_0_0_0.variable_changed(self.epsilon)
-        self.channels_channel_model_0.set_frequency_offset(self.epsilon * 5.89e9 / 10e6)
-        self.pfb_arb_resampler_xxx_0.set_rate(1+self.epsilon)
+        self.channels_channel_model_0.set_frequency_offset((self.epsilon * 5.89e9 / 10e6))
+        self.pfb_arb_resampler_xxx_0.set_rate((1+self.epsilon))
 
     def get_encoding(self):
         return self.encoding
@@ -364,9 +352,6 @@ class wifi_loopback(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=wifi_loopback, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
